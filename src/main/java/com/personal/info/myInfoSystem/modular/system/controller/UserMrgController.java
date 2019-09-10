554e1c63bfb6e95d78716fe9d7d8ff8d4565d8d4
雨一直下 <hs183@qq.com>
2019-09-10 18:04:32 +0800

    添加用户管理

--- src/main/java/com/personal/info/myInfoSystem/modular/system/controller/UserMrgController.java
+++ src/main/java/com/personal/info/myInfoSystem/modular/system/controller/UserMrgController.java
@@ -1,16 +1,21 @@
 package com.personal.info.myInfoSystem.modular.system.controller;
 
+import cn.stylefeng.roses.core.base.controller.BaseController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/mrg")
-public class UserMrgController {
+@RequestMapping("/mgr")
+public class UserMrgController extends BaseController {
 
-    private static final String PREFIX = "/modular/system/user";
+    private static final String PREFIX = "/modular/system/user/";
 
+    /**
+     * 管理员列表
+     * @return
+     */
     @RequestMapping("")
     public String index(){
-        return PREFIX+"/user.html";
+        return PREFIX+"user.html";
     }
 }
