01f99d99dab6ba9abe03e6c8568315b595f26cce
huangshuang <hs183.qq.com>
2020-01-13 11:17:47 +0800

    打成war

--- src/main/java/com/personal/info/myInfoSystem/MyInfoSystemApplication.java
+++ src/main/java/com/personal/info/myInfoSystem/MyInfoSystemApplication.java
@@ -5,15 +5,25 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication(exclude = {WebAutoConfiguration.class})
 @EnableScheduling
-public class MyInfoSystemApplication {
+public class MyInfoSystemApplication extends SpringBootServletInitializer {
 
 	private final static Logger LOGGER = LoggerFactory.getLogger(MyInfoSystemApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(MyInfoSystemApplication.class, args);
 		LOGGER.debug(MyInfoSystemApplication.class.getSimpleName()+" is success!");
+	}
+
+	/**
+	 * 打war包用的
+	 */
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(MyInfoSystemApplication.class);
 	}
 
 }
